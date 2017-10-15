ec4c84ff3205d122f64208c9ca5470d2d6523116
loudywen <loudywen@gmail.com>
2017-10-15 11:41:40 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   template: `<nav class="navbar navbar-default">
+ <div class="container-fluid">
+   <div class="navbar-header">
+     <a class="navbar-brand" href="/">Spring Security Oauth - Password Flow</a>
+   </div>
+ </div>
+</nav>
+<router-outlet></router-outlet>`
 })
-export class AppComponent {
-  title = 'app';
-}
+
+export class AppComponent {}
